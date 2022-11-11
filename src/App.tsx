a9bc1ae3dd4d1bbc7e4d672ba711813630703b4c
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-11 21:54:12 +0300

    create app-wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from "./Technologies";
-import {Header} from "./Header";
-import {Footer} from "./Footer";
 
 const App = () => {
     return (
-        <>
-            <Header/>
-            <Technologies/>
-            <Footer/>
-        </>
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://graphicriver.img.customer.envatousercontent.com/files/383753376/Letter+A+Logo+-+Anglex_1.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=590&s=1322843b6a4dc44c34d61c195d61ee3e"/>
+            </header>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+
+            </nav>
+            <div className="content">Main content</div>
+
+        </div>
 
     );
 }
--- src/Footer.tsx
+++
@@ -1,5 +0,0 @@
-export const Footer=()=>{
-    return (
-        <div>it-kamasutra.com</div>
-    )
-}
