6cda5d0738681eedd6f2a532c20231ee3cdb92c4
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-11 22:05:22 +0300

    add ava + posts

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,17 @@ const App = () => {
                 <div><a>Settings</a></div>
 
             </nav>
-            <div className="content"><img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"/></div>
-
+            <div className="content">
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my posts
+                </div>
+            </div>
         </div>
 
     );
