cedbc40212e3b2f2a0d677b4e9fb69949ddaef98
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-12 18:13:12 +0300

    Create Nav.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
+import {Nav} from "./components/Nav";
 
 const App = () => {
     return (
         <div className="app-wrapper">
-           <Header/>
-            <nav className="nav">
-                <div><a>Profile</a></div>
-                <div><a>Messages</a></div>
-                <div><a>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
-
-            </nav>
+            <Header/>
+            <Nav/>
             <div className="content">
                 <div>
                     <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"/>
