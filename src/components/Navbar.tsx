baf191a0a173a8fa7c4c50a4f4dab224f49cc054
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-12 18:17:16 +0300

    Change name Nav to Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export const Navbar=() => {
-    return( <nav className="nav">
+export const Navbar = () => {
+    return (<nav className="nav">
             <div><a>Profile</a></div>
             <div><a>Messages</a></div>
             <div><a>News</a></div>
