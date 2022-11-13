d403bbfd23b79ad6dc65c2f239bdf6d14ae04c54
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-13 18:20:54 +0300

    create module for Navbar.module.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./Navbar.css"
+import d from "./Navbar.module.css"
 
 export const Navbar = () => {
-    return (<nav className="nav">
-            <div className="waterCreate"><a>Profile</a></div>
-            <div className="waterCreate"><a>Messages</a></div>
-            <div className="waterCreate"><a>News</a></div>
-            <div className="waterCreate"><a>Music</a></div>
-            <div className="waterCreate"><a>Settings</a></div>
+    return (<nav className={d.nav}>
+            <div className={d.item}><a>Profile</a></div>
+            <div className={d.item}><a>Messages</a></div>
+            <div className={d.item}><a>News</a></div>
+            <div className={d.item}><a>Music</a></div>
+            <div className={d.item}><a>Settings</a></div>
 
         </nav>
 
