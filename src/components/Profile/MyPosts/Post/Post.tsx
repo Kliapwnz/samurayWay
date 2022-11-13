0321cacee543809008633ad583bcea902a9a67df
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-13 19:04:23 +0300

    add post

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,11 @@
+import w from "../MyPosts.module.css";
+import React from "react";
+
+export const Post = () => {
+    return (
+        <div className={w.item}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg"/>
+            Post1
+        </div>
+    )
+}
