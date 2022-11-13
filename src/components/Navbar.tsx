c110f3b640eeb80c92fd887375ee1ea81c1618fb
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-13 17:08:47 +0300

    className="waterCreate"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export const Navbar = () => {
     return (<nav className="nav">
-            <div><a>Profile</a></div>
-            <div><a>Messages</a></div>
-            <div><a>News</a></div>
-            <div><a>Music</a></div>
-            <div><a>Settings</a></div>
+            <div className="waterCreate"><a>Profile</a></div>
+            <div className="waterCreate"><a>Messages</a></div>
+            <div className="waterCreate"><a>News</a></div>
+            <div className="waterCreate"><a>Music</a></div>
+            <div className="waterCreate"><a>Settings</a></div>
 
         </nav>
 
