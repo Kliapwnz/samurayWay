d51e59a94d09011e0a48e342b18d888d987530d0
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-17 13:25:43 +0300

    add likes

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -12,7 +12,7 @@ export const Post = (props:postMessageType) => {
             <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg"/>
             {props.message}
             <div>
-                <span>{props.likeCounts}</span>
+                <span>Like {props.likeCounts}</span>
             </div>
         </div>
     )
