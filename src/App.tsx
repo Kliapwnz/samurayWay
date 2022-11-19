21499829633b6dfac08d110ac365a69fb0f926bf
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-19 16:36:04 +0300

    add w.content

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import w from "./components/Profile/Profile.module.css";
 
 const App = () => {
     return (
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            <Dialogs/>
-            {/*<Profile/>*/}
+            <div className={w.content}>
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
+
+
         </div>
 
     );
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,7 @@
 import s from "./Dialogs.module.css"
-
+import w from "../Profile/Profile.module.css"
 export const Dialogs = () => {
     return (
-        <div className={s.Dialogs}>Dialogs</div>
+        <div className={w.content}>Dialogs</div>
     )
 }
