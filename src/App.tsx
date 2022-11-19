208071c8f46ce35c3cb45e095b2d92927cde7b97
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-19 16:29:17 +0300

    add Dialogs.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ const App = () => {
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-            {/*<Profile/>*/}
             <Dialogs/>
+            {/*<Profile/>*/}
         </div>
 
     );
